b6fbcd7a932f95a53bdea0ced70cf40158a180ed
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-07-05 03:20:24 +0530

    Cart Component is Created

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,111 +1,38 @@
-import { RxCross2 } from "react-icons/rx";
+import { MdNavigateNext } from "react-icons/md";
+import { Link } from "react-router-dom";
+import CartItem from "./CartItem";
 
 const Cart = () => {
+  let cartItemAvail = true;
+
   return (
     <div className=" w-full">
-      <div className="max-w-300 mx-auto my-12 flex justify-center flex-row items-start px-4 gap-9">
-        <div className="sec-1 w-[55%]">
-          <div className="heading text-xl font-semibold text-zinc-800">
+      <div className="max-w-400 mx-auto my-10 px-[3vw] font-medium">
+        <div className="nav flex flex-row items-center text-sm text-gray-700 hover:text-gray-600 gap-2">
+          <Link to="/" className=" hover:text-gray-600">
+            Home
+          </Link>
+          <MdNavigateNext className="  hover:text-gray-600" />
+          <div className=" text-gray-800 hover:text-gray-600">Cart</div>
+          <div />
+        </div>
+      </div>
+      <div className="max-w-300 mx-auto my-12 flex justify-center flex-row items-start px-4 gap-9 max-sm:flex-col max-sm:px-2">
+        <div className="sec-1 w-[55%] max-sm:w-full">
+          <div className="heading text-xl font-semibold text-zinc-800 mx-6">
             Shopping Cart
           </div>
           <div className="container-sec mt-3">
-
-            <div className="sec flex flex-row justify-between items-center border-b border-gray-300 py-9 mx-6">
-              <div className="image w-25 h-25 bg-zinc-50 border border-gray-100">
-                <img
-                  src="https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/1.webp"
-                  alt="cart_image"
-                  className="h-full"
-                />
+            {cartItemAvail ? (
+              <CartItem />
+            ) : (
+              <div className="sec flex flex-row justify-between items-center border-b border-gray-300 py-9 mx-6 max-sm:mx-1 max-sm:py-4 bg-gray-100 px-4 mt-6">
+                Cart is Empty..
               </div>
-              <div className="details flex flex-col gap-0.5 text-md">
-                <div className="title font-semibold text-zinc-800">
-                  Gucci Eau De Perfume
-                </div>
-                <div className="category font-medium text-zinc-800">
-                  Perfume
-                </div>
-                <div className="identity text-zinc-800">#BEA-NAI-NAI-005</div>
-              </div>
-              <div className="addToBtn flex text-md">
-                <div className="px-3 py-1 border border-gray-200 bg-gray-50 hover:bg-white cursor-pointer">
-                  -
-                </div>
-                <div className="px-3 py-1 border  border-gray-200">2</div>
-                <div className="px-3 py-1 border border-gray-200 bg-gray-50 hover:bg-white cursor-pointer">
-                  +
-                </div>
-              </div>
-              <div className="price font-semibold">$153</div>
-              <div className="delBtn text-lg text-zinc-500 cursor-pointer hover:text-red-400">
-                <RxCross2 />
-              </div>
-            </div>
-           <div className="sec flex flex-row justify-between items-center border-b border-gray-300 py-9 mx-6">
-              <div className="image w-25 h-25 bg-zinc-50 border border-gray-100">
-                <img
-                  src="https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/1.webp"
-                  alt="cart_image"
-                  className="h-full"
-                />
-              </div>
-              <div className="details flex flex-col gap-0.5 text-md">
-                <div className="title font-semibold text-zinc-800">
-                  Gucci Eau De Perfume
-                </div>
-                <div className="category font-medium text-zinc-800">
-                  Perfume
-                </div>
-                <div className="identity text-zinc-800">#BEA-NAI-NAI-005</div>
-              </div>
-              <div className="addToBtn flex text-md">
-                <div className="px-3 py-1 border border-gray-200 bg-gray-50 hover:bg-white cursor-pointer">
-                  -
-                </div>
-                <div className="px-3 py-1 border  border-gray-200">2</div>
-                <div className="px-3 py-1 border border-gray-200 bg-gray-50 hover:bg-white cursor-pointer">
-                  +
-                </div>
-              </div>
-              <div className="price font-semibold">$153</div>
-              <div className="delBtn text-lg text-zinc-500 cursor-pointer hover:text-red-400">
-                <RxCross2 />
-              </div>
-            </div>
-            <div className="sec flex flex-row justify-between items-center border-b border-gray-300 py-9 mx-6">
-              <div className="image w-25 h-25 bg-zinc-50 border border-gray-100">
-                <img
-                  src="https://cdn.dummyjson.com/product-images/fragrances/chanel-coco-noir-eau-de/1.webp"
-                  alt="cart_image"
-                  className="h-full"
-                />
-              </div>
-              <div className="details flex flex-col gap-0.5 text-md">
-                <div className="title font-semibold text-zinc-800">
-                  Gucci Eau De Perfume
-                </div>
-                <div className="category font-medium text-zinc-800">
-                  Perfume
-                </div>
-                <div className="identity text-zinc-800">#BEA-NAI-NAI-005</div>
-              </div>
-              <div className="addToBtn flex text-md">
-                <div className="px-3 py-1 border border-gray-200 bg-gray-50 hover:bg-white cursor-pointer">
-                  -
-                </div>
-                <div className="px-3 py-1 border  border-gray-200">2</div>
-                <div className="px-3 py-1 border border-gray-200 bg-gray-50 hover:bg-white cursor-pointer">
-                  +
-                </div>
-              </div>
-              <div className="price font-semibold">$153</div>
-              <div className="delBtn text-lg text-zinc-500 cursor-pointer hover:text-red-400">
-                <RxCross2 />
-              </div>
-            </div>
+            )}
           </div>
         </div>
-        <div className="sec-2 border border-gray-200 rounded-xl w-[40%] h-full flex flex-col items-center py-12 gap-4">
+        <div className="sec-2 border border-gray-200 rounded-xl w-[40%] h-full flex flex-col items-center py-12 gap-4 max-sm:w-[95vw] max-sm:mb-10">
           <div className="heading w-80 text-lg font-semibold text-zinc-800">
             Order Summary
           </div>
